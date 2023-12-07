509b17440edcd98737189f184ff7bea2356cdc23
TomXiaoYZ <tom.xiao@hotmail.com>
2023-12-07 16:46:11 +0800

    openldap

--- package/dep-auth/dep-auth-dao/src/main/java/com/dep/auth/dao/mapper/UserSessionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dep.auth.dao.mapper;
-
-import tk.mybatis.mapper.common.BaseMapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface UserSessionMapper extends BaseMapper<UserSessionMapper>, MySqlMapper<UserSessionMapper> {
-
-}
